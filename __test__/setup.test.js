88f2503c62d75d4ed713c51e5cbfa3cf7af80666
Rocio Matias Duran <rocio@gendra.mx>
2020-07-07 07:43:50 -0500

    [UPDATE] Se realiza corrección en el archivo de septup

--- __test__/setup.test.js
+++ __test__/setup.test.js
@@ -1,12 +1,12 @@
 // Despues de cada prueba
-afterEach(()=> console.log('Despues de cada prueba'));
-afterAll(()=> console.log('Despues de todas las pruebas'));
+afterEach(() => console.log('Despues de cada prueba'));
+afterAll(() => console.log('Despues de todas las pruebas'));
 
 // Antes de cada prueba
-beforeEach(()=> console.log('Antes de cada prueba'));
-beforeAll(()=> console.log('Antes de cada prueba'));
+beforeEach(() => console.log('Antes de cada prueba'));
+beforeAll(() => console.log('Antes de todas las pruebas'));
 
-describe('Probar antes de ejecutar', () =>{
+describe('Probar antes de ejecutar', () => {
     test('Es verdadero', () => {
         expect(true).toBeTruthy();
     });
